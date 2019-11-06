248e4aa23515f8581b852fc02d6f7b4d23c1db7d
Matt Stanford <mds6058@gmail.com>
2019-11-05 21:49:12 -0500

    Fixed script so it always searches for the app from the script's file location

--- ghosthand_install.sh
+++ ghosthand_install.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 #First find the app in the wrapper framework
-appLocation=`find . -name "GhostHand.app" | head -n 1`
+scriptLocation=`dirName $0`
+appLocation=`find $scriptLocation -name "GhostHand.app" | head -n 1`
 
 #First boot the simulator we are running against
 xcrun simctl boot "${TARGET_DEVICE_IDENTIFIER}"
 
 #Install the app
+echo xcrun simctl install ${TARGET_DEVICE_IDENTIFIER} $appLocation
 xcrun simctl install ${TARGET_DEVICE_IDENTIFIER} $appLocation
+
+#Install on any running clones managed by Xcode (if running test in parallel)
 xcrun simctl --set testing install booted $appLocation
 
 
