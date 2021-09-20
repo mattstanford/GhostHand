819bf1bd4afc8891439d9e9554a0b3601b8ed88a
Matt Stanford <mds6058@gmail.com>
2021-09-20 16:00:07 -0400

    Initial SPM support

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "GhostHand",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "GhostHandLib",
+            targets: ["GhostHandLib"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "GhostHandLib",
+            resources: [
+                .copy("GhostHand/GhostHandLib/Built_Product/*")
+                .copy("ghosthand_install.sh")
+            ]
+        )
+    ]
+)
