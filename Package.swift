70fc5ce56393be3e4aeca44a104269da352d0892
Matt Stanford <mds6058@gmail.com>
2021-09-20 16:02:10 -0400

    Updated SPM minimum version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "GhostHand",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
