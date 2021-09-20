21fd67f7bbda2b138fb8242731338aae2422ea6f
Matt Stanford <mds6058@gmail.com>
2021-09-20 16:13:50 -0400

    Added alternative path property for SPM sources

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GhostHandLib",
+            path: "GhostHand/GhostHandLib/Classes/",
             resources: [
                 .copy("GhostHand/GhostHandLib/Built_Product/GhostHand.app"),
                 .copy("ghosthand_install.sh")
